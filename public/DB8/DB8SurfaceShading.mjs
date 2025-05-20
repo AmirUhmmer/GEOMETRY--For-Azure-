4cc092f214e02ae7d897606cc400000ae9e64c0a
AmirUhmmer <jazmirjulio@gmail.com>
2025-05-20 13:32:37 +0200

    optimized heatmap rendering

--- public/DB8/DB8SurfaceShading.mjs
+++ public/DB8/DB8SurfaceShading.mjs
@@ -33,16 +33,20 @@ export async function HEATMAP(viewer, selectedFloor) {
                         let propertyPromises = dbIDs.map(dbId => {
                             return new Promise((propResolve) => {
                                 model.getProperties(dbId, props => {
-                                    console.log(props);
-                                    const systemCodeProperty = props.properties.find(prop => prop.displayName === 'System Code');
+                                    // console.log(props);
+                                    //const systemCodeProperty = props.properties.find(prop => prop.displayName === 'System Code');
 
-                                    if (systemCodeProperty && systemCodeProperty.displayValue == systemCodeValue) {
+                                    //if (systemCodeProperty && systemCodeProperty.displayValue == systemCodeValue) {
+                                    // props.dbId === 24382 ? console.log('properties (GYM): ' & props) : console.log('No dbId found'); 
+                                    if (props.name === nodesData[index].modelName) {
+                                        console.log(`Properties for dbId ${dbId}:`, props.name);
                                         // Update the existing nodesData entry with the new dbId and other properties
                                         nodesData[index] = {
                                             name: node.name,
                                             dbId: dbId,
                                             systemCode: systemCodeValue,
-                                            guid: node.guid // Retaining the original guid if needed
+                                            guid: node.guid, // Retaining the original guid if needed
+                                            modelName: props.name
                                         };
 
                                         //console.log(`Found matching dbId: ${dbId} for asset: ${node.name} with systemCode: ${systemCodeValue}`);
@@ -73,53 +77,54 @@ export async function HEATMAP(viewer, selectedFloor) {
     if (selectedFloor === 1) {
         console.log('Rendering heatmap for the First Floor');
         nodesData = [
-        { name: "Main Entrance (DB8.-.1.001)"       , dbId: 25788, guid: "b957051a-2cb0-4bcb-990c-3c34a6f627a7" }, //uid: "b957051a-2cb0-4bcb-990c-3c34a6f627a7"
+        { name: "Main Entrance (DB8.-.1.001)"       , dbId: 25788, guid: "b957051a-2cb0-4bcb-990c-3c34a6f627a7", modelName: "Floor [1885372]" }, //uid: "b957051a-2cb0-4bcb-990c-3c34a6f627a7"
         //{ name: "Entrance Area (DB8.-.1.002)"       , dbId: 25874, guid: "d9063147-d93d-4746-858b-e45d810d17d5" },
-        { name: "Small Meeting/Office (DB8.-.1.004)", dbId: 25524, guid: "3b69ceda-a9e0-4b04-8a92-7ecccb6286a0" },
-        { name: "Office (DB8.-1.005)"              , dbId: 25505, guid: "b343e138-36fb-4af4-80be-d266aca41db9" },
-        { name: "Office (DB8.-1.006)"              , dbId: 25486, guid: "7a97be73-e5f6-422b-ade9-6a14ca78b879" },
-        { name: "Office (DB8.-1.007)"              , dbId: 25435, guid: "2c570a32-6987-4f6a-bac2-09572d9bcf56" },
-        { name: "Hot Desk (DB8.-.1.008)"            , dbId: 25853, guid: "c34c8e2d-67fe-49dc-af3f-4af4991e3f02" },
-        { name: "Office (DB8.-1.009)"              , dbId: 25452, guid: "d695fb48-8698-41b3-a557-8dbb69524211" },
-        { name: "Office (DB8.-1.010)"              , dbId: 25466, guid: "d089bfc1-5b63-48d2-9c79-2ee5162b342e" },
-        { name: "Office (DB8.-1.011)"              , dbId: 25580, guid: "fa29b828-4a93-4f89-91c7-0afc49c37ee1" },
-        { name: "Office (DB8.-1.012)"              , dbId: 25600, guid: "bcf5cba9-1821-46c9-90e4-737860933e69" },
-        { name: "Office (DB8.-1.013)"              , dbId: 25615, guid: "a674dc37-bd9a-4afe-a8e0-a04954293334" },
-        { name: "Office (DB8.-1.014)"              , dbId: 25635, guid: "f545cc16-709c-46ca-8331-41463edead9f" },
-        { name: "Social Area (DB8.-.1.017)"         , dbId: 25924, guid: "1da69c37-4364-4bdd-a40d-ec60078c5593" },
-        { name: "Office (DB8.-1.018)"              , dbId: 25419, guid: "1d512e32-54df-4c23-ac36-25fb03665dfd" },
-        { name: "Office (DB8.-1.019)"              , dbId: 25400, guid: "d7b28135-4a1e-4251-b436-b21b630d0012" },
-        { name: "Office (DB8.-1.020)"              , dbId: 25389, guid: "ce717192-968f-46b5-bb43-6fb73965da0f" },
-        { name: "Electronics Workshop (DB8.-1.021)", dbId: 25376, guid: "2da239f8-ddab-4d43-823f-8797638123f2" },
-        { name: "Mechanical Workshop (DB8.-1.022)" , dbId: 25383, guid: "a3215104-8c66-4714-96f4-dcb5b3fb2ab2" },
+        { name: "Small Meeting/Office (DB8.-.1.004)", dbId: 25524, guid: "3b69ceda-a9e0-4b04-8a92-7ecccb6286a0", modelName: "Floor [1870026]" },
+        { name: "Office (DB8.-1.005)"              , dbId: 25505, guid: "b343e138-36fb-4af4-80be-d266aca41db9", modelName: "Floor [1868278]" },
+        { name: "Office (DB8.-1.006)"              , dbId: 25486, guid: "7a97be73-e5f6-422b-ade9-6a14ca78b879", modelName: "Floor [1866802]" },
+        { name: "Office (DB8.-1.007)"              , dbId: 25435, guid: "2c570a32-6987-4f6a-bac2-09572d9bcf56", modelName: "Floor [1862380]" },
+        { name: "Hot Desk (DB8.-.1.008)"            , dbId: 25853, guid: "c34c8e2d-67fe-49dc-af3f-4af4991e3f02", modelName: "Floor [1890547]" },
+        { name: "Office (DB8.-1.009)"              , dbId: 25452, guid: "d695fb48-8698-41b3-a557-8dbb69524211", modelName: "Floor [1863630]" },
+        { name: "Office (DB8.-1.010)"              , dbId: 25466, guid: "d089bfc1-5b63-48d2-9c79-2ee5162b342e", modelName: "Floor [1865073]" },
+        { name: "Office (DB8.-1.011)"              , dbId: 25580, guid: "fa29b828-4a93-4f89-91c7-0afc49c37ee1", modelName: "Floor [1872322]" },
+        { name: "Office (DB8.-1.012)"              , dbId: 25600, guid: "bcf5cba9-1821-46c9-90e4-737860933e69", modelName: "Floor [1874087]" },
+        { name: "Office (DB8.-1.013)"              , dbId: 25615, guid: "a674dc37-bd9a-4afe-a8e0-a04954293334", modelName: "Floor [1875634]" },
+        { name: "Office (DB8.-1.014)"              , dbId: 25635, guid: "f545cc16-709c-46ca-8331-41463edead9f", modelName: "Floor [1877095]" },
+        { name: "Social Area (DB8.-.1.017)"         , dbId: 25924, guid: "1da69c37-4364-4bdd-a40d-ec60078c5593", modelName: "Floor [1897010]" },
+        { name: "Office (DB8.-1.018)"              , dbId: 25419, guid: "1d512e32-54df-4c23-ac36-25fb03665dfd", modelName: "Floor [1860949]" },
+        { name: "Office (DB8.-1.019)"              , dbId: 25400, guid: "d7b28135-4a1e-4251-b436-b21b630d0012", modelName: "Floor [1859489]" },
+        { name: "Office (DB8.-1.020)"              , dbId: 25389, guid: "ce717192-968f-46b5-bb43-6fb73965da0f", modelName: "Floor [1858458]" },
+        { name: "Electronics Workshop (DB8.-1.021)", dbId: 23552, guid: "2da239f8-ddab-4d43-823f-8797638123f2", modelName: "Floor [1856561]" },
+        { name: "Mechanical Workshop (DB8.-1.022)" , dbId: 23559, guid: "a3215104-8c66-4714-96f4-dcb5b3fb2ab2", modelName: "Floor [1857330]" },
 
         ];
         await searchForSystemCode(viewer, nodesData); // Wait for search to complete
     } else if (selectedFloor === 2) {
         console.log('Rendering heatmap for the Second Floor');
         nodesData = [
-            { name: "Social Area (DB8.-.2.001)", dbId: 26840, guid: "193d3a83-b870-4d7b-83eb-ea73111b8c0a" },
-            { name: "Office (DB8.-2.002)", dbId: 26075, guid: "d8d4faa1-2bc9-4809-8a25-08430813fc63" },
-            { name: "Office (DB8.-2.003)", dbId: 26084, guid: "08d4abfb-ca40-4a35-91b4-3582388be8e5" },
-            { name: "Office (DB8.-2.004)", dbId: 26102, guid: "23600d1e-3798-4b77-8eaa-fede3c1bddc8" },
-            { name: "Office (DB8.-2.005)", dbId: 26122, guid: "dbe72c0e-6856-4a84-8604-e33a06914608" },
-            { name: "Office (DB8.-2.006)", dbId: 26142, guid: "5d186d87-62d6-415e-8f23-7ed4f96439dc" },
-            { name: "Office (DB8.-2.007)", dbId: 26161, guid: "dede5f73-0a26-4330-a7b9-52499d79cc71" },
-            { name: "Office (DB8.-2.008)", dbId: 26181, guid: "671c9530-5a8c-49ce-aa4c-28e752c8137e" },
-            { name: "Office (DB8.-2.009)", dbId: 26201, guid: "5c4c68f2-8d13-4b67-b7aa-88c876e12ff0" },
-            { name: "Office (DB8.-2.010)", dbId: 26216, guid: "f378f3bf-b905-4928-94d4-ce7dc0baffd0" },
-            { name: "Office (DB8.-2.011)", dbId: 26247, guid: "282a8fdc-d284-43ec-bf87-cb4f0b6b22f9" },
-            { name: "Office (DB8.-2.012)", dbId: 26269, guid: "b8b962ae-4e0a-4563-848c-04a081e4700e" },
-            { name: "Office (DB8.-2.013)", dbId: 26284, guid: "008bc641-423d-4d53-bf0e-0046218da1c4" },
-            { name: "Office (DB8.-2.014)", dbId: 26303, guid: "7a2568ce-4143-427e-9a7d-18d945d3a12b" },
-            { name: "Office (DB8.-2.015)", dbId: 26322, guid: "9d3aefd9-2b96-4674-b4dd-0a7947117af5" },
-            { name: "Office (DB8.-2.016)", dbId: 26340, guid: "0e6cd4f0-cdb4-4c1e-8277-9fe15073f7ba" },
-            { name: "Office (DB8.-2.017)", dbId: 26359, guid: "c3c4b142-1d49-4667-87e2-b9fd7ad3afb1" },
-            { name: "Office (DB8.-2.018)", dbId: 26376, guid: "1147a344-9089-469f-a51b-ccc7b8b431cc" },
-            { name: "Office (DB8.-2.019)", dbId: 26393, guid: "5bd45fab-d1ce-401a-9671-f0dd25b39c9e" },
-            { name: "Office (DB8.-2.020)", dbId: 26412, guid: "310a64d0-dc6c-4ce4-9730-78301272ea99" },
-            { name: "Meeting Room (DB8.-.2.021)", dbId: 26443, guid: "16bc9315-dd0e-460e-a9c3-10ae5602dd2d" },
-            { name: "Gym/Fitness Room (DB8.-.2.022)", dbId: 26432, guid: "34ad5978-9645-4d9e-a519-6cff67eb1713" },
+            { name: "Social Area (DB8.-.2.001)", dbId: 26840, guid: "193d3a83-b870-4d7b-83eb-ea73111b8c0a", modelName: "Floor [1960611]" },
+            { name: "Office (DB8.-2.002)", dbId: 26075, guid: "d8d4faa1-2bc9-4809-8a25-08430813fc63", modelName: "Floor [1909924]" },
+            { name: "Office (DB8.-2.003)", dbId: 26084, guid: "08d4abfb-ca40-4a35-91b4-3582388be8e5", modelName: "Floor [1910861]" },
+            { name: "Office (DB8.-2.004)", dbId: 26102, guid: "23600d1e-3798-4b77-8eaa-fede3c1bddc8", modelName: "Floor [1912374]" },
+            { name: "Office (DB8.-2.005)", dbId: 26122, guid: "dbe72c0e-6856-4a84-8604-e33a06914608", modelName: "Floor [1913686]" },
+            { name: "Office (DB8.-2.006)", dbId: 26142, guid: "5d186d87-62d6-415e-8f23-7ed4f96439dc", modelName: "Floor [1914881]" },
+            { name: "Office (DB8.-2.007)", dbId: 26161, guid: "dede5f73-0a26-4330-a7b9-52499d79cc71", modelName: "Floor [1916249]" },
+            { name: "Office (DB8.-2.008)", dbId: 26181, guid: "671c9530-5a8c-49ce-aa4c-28e752c8137e", modelName: "Floor [1917284]" },
+            { name: "Office (DB8.-2.009)", dbId: 26201, guid: "5c4c68f2-8d13-4b67-b7aa-88c876e12ff0", modelName: "Floor [1918244]" },
+            { name: "Office (DB8.-2.010)", dbId: 26216, guid: "f378f3bf-b905-4928-94d4-ce7dc0baffd0", modelName: "Floor [1919270]" },
+            { name: "Office (DB8.-2.011)", dbId: 26247, guid: "282a8fdc-d284-43ec-bf87-cb4f0b6b22f9", modelName: "Floor [1921371]" },
+            { name: "Office (DB8.-2.012)", dbId: 26269, guid: "b8b962ae-4e0a-4563-848c-04a081e4700e", modelName: "Floor [1923498]" },
+            { name: "Office (DB8.-2.013)", dbId: 26284, guid: "008bc641-423d-4d53-bf0e-0046218da1c4", modelName: "Floor [1924504]" },
+            { name: "Office (DB8.-2.014)", dbId: 26303, guid: "7a2568ce-4143-427e-9a7d-18d945d3a12b", modelName: "Floor [1925617]" },
+            { name: "Office (DB8.-2.015)", dbId: 26322, guid: "9d3aefd9-2b96-4674-b4dd-0a7947117af5", modelName: "Floor [1926599]" },
+            { name: "Office (DB8.-2.016)", dbId: 26340, guid: "0e6cd4f0-cdb4-4c1e-8277-9fe15073f7ba", modelName: "Floor [1927938]" },
+            { name: "Office (DB8.-2.017)", dbId: 26359, guid: "c3c4b142-1d49-4667-87e2-b9fd7ad3afb1", modelName: "Floor [1929094]" },
+            { name: "Office (DB8.-2.018)", dbId: 26376, guid: "1147a344-9089-469f-a51b-ccc7b8b431cc", modelName: "Floor [1930249]" },
+            { name: "Office (DB8.-2.019)", dbId: 26393, guid: "5bd45fab-d1ce-401a-9671-f0dd25b39c9e", modelName: "Floor [1931340]" },
+            { name: "Office (DB8.-2.020)", dbId: 26412, guid: "310a64d0-dc6c-4ce4-9730-78301272ea99", modelName: "Floor [1932440]" },
+            { name: "Meeting Room (DB8.-.2.021)", dbId: 24382, guid: "16bc9315-dd0e-460e-a9c3-10ae5602dd2d", modelName: "Floor [1934689]" },
+            { name: "Gym/Fitness Room (DB8.-.2.022)", dbId: 24382, guid: "34ad5978-9645-4d9e-a519-6cff67eb1713", modelName: "Floor [1933386]" },
+            
         ];
         await searchForSystemCode(viewer, nodesData); // Wait for search to complete
     }
@@ -127,8 +132,8 @@ export async function HEATMAP(viewer, selectedFloor) {
     console.log('Rendering heatmap for U1');
     nodesData = [
         // { name: "Storage (DB8.-.U1.002)", dbId: 26840, guid: "" },
-        { name: "Tenant Area (DB8.-.U1.007)", dbId: 27048, guid: "507ac9f4-73a7-4034-8cf5-33b9e5e9fedb" },
-        { name: "Tenant Area (DB8.-.U1.007)", dbId: 27032, guid: "507ac9f4-73a7-4034-8cf5-33b9e5e9fedb" },
+        { name: "Tenant Area (DB8.-.U1.007)", dbId: 27048, guid: "507ac9f4-73a7-4034-8cf5-33b9e5e9fedb", modelName: "Floor [1973465]" },
+        { name: "Tenant Area (DB8.-.U1.007)", dbId: 27032, guid: "507ac9f4-73a7-4034-8cf5-33b9e5e9fedb", modelName: "Floor [1972498]" },
         // { name: "Workshop (DB8.-.U1.011)", dbId: 26840, guid: "" },
     ];
     await searchForSystemCode(viewer, nodesData); // Wait for search to complete
@@ -211,65 +216,23 @@ export async function HEATMAP(viewer, selectedFloor) {
 
 //             ACTUAL FUNCTION TO GET DATA FROM DATABASE
 
-async function getSensorValue(location) {
-    try {
-        const response = await fetch(`/api/sensor/${location}`);
-        if (!response.ok) {
-            throw new Error(`Error fetching sensor data: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        const temp = parseInt(data.value); // Converts data.value to an integer
-        const minTemp = 16;
-        const maxTemp = 24;
-
-        // Normalize the temperature value to range [0, 1]
-        // console.log(`Sensor value for ${location}: ${data.value}`);
-        const result = Math.min(Math.max((temp - minTemp) / (maxTemp - minTemp), 0), 1);
-        return result;
-
-
-        //18 below celsius to blue 
-        //19 up green 20-23
-        //22 transition to red
-
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-
-
-
-
-
-
-// SAMPLE DATA 
-
 // async function getSensorValue(location) {
 //     try {
-//         // Comment out the actual fetch request for now
-//         /*
 //         const response = await fetch(`/api/sensor/${location}`);
 //         if (!response.ok) {
 //             throw new Error(`Error fetching sensor data: ${response.statusText}`);
 //         }
+
 //         const data = await response.json();
 //         const temp = parseInt(data.value); // Converts data.value to an integer
-//         */
-
-//         // Generate random temperature between 16 and 25
-//         const randomTemp = Math.floor(Math.random() * (25 - 16 + 1)) + 16;
-//         const sampleData = { value: randomTemp };
-//         const temp = parseInt(sampleData.value);
-
 //         const minTemp = 16;
 //         const maxTemp = 24;
 
 //         // Normalize the temperature value to range [0, 1]
-//         // console.log(`Sensor value for ${location}: ${sampleData.value}`);
+//         // console.log(`Sensor value for ${location}: ${data.value}`);
 //         const result = Math.min(Math.max((temp - minTemp) / (maxTemp - minTemp), 0), 1);
 //         return result;
+
 
 //         //18 below celsius to blue 
 //         //19 up green 20-23
@@ -279,6 +242,48 @@ async function getSensorValue(location) {
 //         console.error(err);
 //     }
 // }
+
+
+
+
+
+
+
+// SAMPLE DATA 
+
+async function getSensorValue(location) {
+    try {
+        // Comment out the actual fetch request for now
+        /*
+        const response = await fetch(`/api/sensor/${location}`);
+        if (!response.ok) {
+            throw new Error(`Error fetching sensor data: ${response.statusText}`);
+        }
+        const data = await response.json();
+        const temp = parseInt(data.value); // Converts data.value to an integer
+        */
+
+        // Generate random temperature between 16 and 25
+        const randomTemp = Math.floor(Math.random() * (25 - 16 + 1)) + 16;
+        const sampleData = { value: randomTemp };
+        const temp = parseInt(sampleData.value);
+
+        const minTemp = 16;
+        const maxTemp = 24;
+
+        // Normalize the temperature value to range [0, 1]
+        // console.log(`Sensor value for ${location}: ${sampleData.value}`);
+        const result = Math.min(Math.max((temp - minTemp) / (maxTemp - minTemp), 0), 1);
+        return result;
+
+        //18 below celsius to blue 
+        //19 up green 20-23
+        //22 transition to red
+
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 
 
